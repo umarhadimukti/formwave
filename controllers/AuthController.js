a830428be4979088084c3dc9c57c6e3aa04705b5
umarhadimukti <umarhadimukti@gmail.com>
2024-03-13 13:05:53 +0700

    style: auth syntax

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -3,7 +3,8 @@ import { User } from '../models/User.js';
 export default class AuthController {
   static async register(req, res) {
     try {
-      const user = await User.create(req.body);
+      const user = await new User(req.body);
+      user.save();
       return res.status(201).json({message: 'Berhasil mendaftar!', data: user});
     } catch (err) {
       return res.status(500).json({message: err.message});
